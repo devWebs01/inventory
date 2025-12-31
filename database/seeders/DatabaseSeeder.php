f627393643e4a4a000df14ffad1aa0ae93ff2f46
devWebs01 <testingbae66@gmail.com>
2025-12-31 14:30:17 +0700

    chore: ignore .claude directory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,54 +20,54 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(10)->create();
 
-        // $this->call(ShieldSeeder::class);
+        $this->call(ShieldSeeder::class);
 
-        // $faker = Faker::create();
+        $faker = Faker::create();
 
-        // /**
-        //  * 1. Pastikan role super_admin ada
-        //  */
-        // $superAdminRole = Role::firstOrCreate([
-        //     'name' => 'super_admin',
-        //     'guard_name' => 'web',
-        // ]);
+        /**
+         * 1. Pastikan role super_admin ada
+         */
+        $superAdminRole = Role::firstOrCreate([
+            'name' => 'super_admin',
+            'guard_name' => 'web',
+        ]);
 
-        // /**
-        //  * 2. Buat / ambil user super admin
-        //  */
-        // $superAdmin = User::firstOrCreate(
-        //     ['email' => 'admin@testing.com'],
-        //     [
-        //         'name' => 'Super Admin',
-        //         'email_verified_at' => now(),
-        //         'password' => Hash::make('password'),
-        //     ]
-        // );
+        /**
+         * 2. Buat / ambil user super admin
+         */
+        $superAdmin = User::firstOrCreate(
+            ['email' => 'admin@testing.com'],
+            [
+                'name' => 'Super Admin',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+            ]
+        );
 
-        // /**
-        //  * 3. Assign role super_admin
-        //  */
-        // if (! $superAdmin->hasRole('super_admin')) {
-        //     $superAdmin->assignRole($superAdminRole);
-        // }
+        /**
+         * 3. Assign role super_admin
+         */
+        if (!$superAdmin->hasRole('super_admin')) {
+            $superAdmin->assignRole($superAdminRole);
+        }
 
-        // /**
-        //  * 4. Generate user lain untuk role selain super_admin
-        //  */
-        // $roles = Role::where('name', '!=', 'super_admin')->get();
+        /**
+         * 4. Generate user lain untuk role selain super_admin
+         */
+        $roles = Role::where('name', '!=', 'super_admin')->get();
 
-        // foreach ($roles as $role) {
-        //     for ($i = 0; $i < 10; $i++) {
-        //         $user = User::create([
-        //             'firstname' => $faker->firstName,
-        //             'lastname' => $faker->lastName,
-        //             'email' => $faker->unique()->safeEmail,
-        //             'email_verified_at' => now(),
-        //             'password' => Hash::make('password'),
-        //         ]);
+        foreach ($roles as $role) {
+            for ($i = 0; $i < 10; $i++) {
+                $user = User::create([
+                    'firstname' => $faker->firstName,
+                    'lastname' => $faker->lastName,
+                    'email' => $faker->unique()->safeEmail,
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('password'),
+                ]);
 
-        //         $user->assignRole($role);
-        //     }
-        // }
+                $user->assignRole($role);
+            }
+        }
     }
 }
